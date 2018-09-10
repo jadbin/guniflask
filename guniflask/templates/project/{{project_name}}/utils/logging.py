e7a0195700aafedf9c95f675b7f628e6e0109ff1
jadbin <jadbin.com@hotmail.com>
2018-09-10 22:46:18 +0800

    update logging

--- guniflask/templates/project/{{project_name}}/utils/logging.py
+++ guniflask/templates/project/{{project_name}}/utils/logging.py
@@ -3,12 +3,12 @@
 import logging
 
 
-def configure_logger(name, logger):
+def redirect_logger(name, logger):
     log = logging.getLogger(name)
     log.handlers = logger.handlers
     log.setLevel(logger.level)
 
 
-def configure_app_logger(app, logger):
+def redirect_app_logger(app, logger):
     app.logger.handlers = logger.handlers
     app.logger.setLevel(logger.level)
