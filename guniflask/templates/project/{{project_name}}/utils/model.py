a37dbef4387bbb3329a4e33d00f0a42265f45c98
jadbin <jadbin.com@hotmail.com>
2018-09-12 11:16:33 +0800

    add tools

---
+++ guniflask/templates/project/{{project_name}}/utils/model.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+import sqlalchemy
+
+__all__ = ['model_to_dict', 'dict_to_model']
+
+
+def model_to_dict(model):
+    col_attrs = sqlalchemy.inspect(model).mapper.column_attrs
+    return {c.key: getattr(model, c.key) for c in col_attrs}
+
+
+def dict_to_model(dict_obj, model_cls=None):
+    if model_cls is None:
+        return dict(dict_obj)
+    col_attrs = sqlalchemy.inspect(model_cls).mapper.column_attrs
+    kwargs = {c.key: dict_obj[c.key] for c in col_attrs if c.key in dict_obj}
+    model = model_cls(**kwargs)
+    return model
