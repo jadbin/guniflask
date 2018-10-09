f755412cf864f2673160c2ada8346cabb5ae04d3
jadbin <jadbin.com@hotmail.com>
2018-10-09 16:33:28 +0800

    add test

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -141,3 +141,14 @@ def test_delete():
     assert len(c) == 1 and 'k1' not in c
     del c['k3']
     assert len(c) == 0 and 'k3' not in c
+
+
+def test_setdefault():
+    c = Settings()
+    c.setdefault('k1', 'v1')
+    assert c['k1'] == 'v1'
+    c.setdefault('k1', 'v2')
+    assert c['k1'] == 'v1'
+    c['k2'] = None
+    c.setdefault('k2', 'v2')
+    assert c['k2'] is 'v2'
