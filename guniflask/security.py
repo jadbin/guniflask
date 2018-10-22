bdae9049744d696589c0166b70ffbfcdedfe5fd5
jadbin <jadbin.com@hotmail.com>
2018-10-22 23:49:55 +0800

    update authentication

--- guniflask/security.py
+++ guniflask/security.py
@@ -1,30 +1,33 @@
 # coding=utf-8
 
 import datetime as dt
+from functools import wraps
 
-from flask import current_app, _request_ctx_stack, request
+from flask import current_app, _request_ctx_stack, request, abort
 from jwt import InvalidTokenError
 from werkzeug.local import LocalProxy
 
 from guniflask.utils.security import encode_jwt, decode_jwt
+
+auth_manager = LocalProxy(lambda: current_app.extensions['auth_manager'])
 
 
 def _load_user():
     ctx = _request_ctx_stack.top
     if ctx is not None:
         if not hasattr(ctx, 'user'):
-            user = current_app.extensions['auth_manager'].load_user()
+            user = auth_manager.load_user()
             ctx.user = user
         return ctx.user
 
 
 current_user = LocalProxy(_load_user)
-auth_manager = LocalProxy(lambda: current_app.extensions['auth_manager'])
 
 
 class AuthManager:
     def __init__(self, app=None):
         self._user_loader = None
+        self._unauthorized_handler = None
         if app:
             self.init_app(app)
 
@@ -39,6 +42,16 @@ class AuthManager:
         if self._user_loader is None:
             raise RuntimeError('Missing user loader')
         return self._user_loader()
+
+    @property
+    def unauthorized_view(self):
+        if self._unauthorized_handler:
+            return self._unauthorized_handler()
+        return abort(401)
+
+    def unauthorized(self, func):
+        self._unauthorized_handler = func
+        return func
 
 
 class User:
@@ -94,6 +107,42 @@ class AnonymousUser(User):
 
     def has_any_role(self, *roles):
         return False
+
+
+def login_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if not current_user.is_authenticated:
+            return auth_manager.unauthorized_view
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def roles_required(*roles):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if current_user is None or not current_user.has_any_role(*roles):
+                return auth_manager.unauthorized_view
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+def authorities_required(*authorities):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if current_user is None or not current_user.has_any_authority(*authorities):
+                return auth_manager.unauthorized_view
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
 
 
 class JwtAuthManager(AuthManager):
