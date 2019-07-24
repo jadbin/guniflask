a6e814abc464ceafe0a48b1ef4bfcd32156dc227
jadbin <jadbin.com@hotmail.com>
2019-07-24 16:04:49 +0800

    fix default env

--- guniflask/manage.py
+++ guniflask/manage.py
@@ -2,12 +2,13 @@
 
 import sys
 import os
-from os.path import isfile, join, dirname, exists
+from os.path import isfile, join, dirname, exists, isdir
 import argparse
 import signal
 import time
 import multiprocessing
 from importlib import import_module
+import json
 
 from sqlalchemy.engine import create_engine
 from sqlalchemy.schema import MetaData
@@ -25,10 +26,14 @@ from guniflask.app import create_app
 def set_environ():
     home_dir = os.environ.get('GUNIFLASK_HOME')
     if home_dir is None:
-        home_dir = dirname(dirname(__file__))
+        home_dir = os.getcwd()
         os.environ['GUNIFLASK_HOME'] = home_dir
     if home_dir not in sys.path:
         sys.path.append(home_dir)
+    if 'GUNIFLASK_PROJECT_NAME' not in os.environ:
+        project_name = _infer_project_name(home_dir)
+        if project_name:
+            os.environ['GUNIFLASK_PROJECT_NAME'] = project_name
     if 'GUNIFLASK_CONF_DIR' not in os.environ:
         os.environ['GUNIFLASK_CONF_DIR'] = join(home_dir, 'conf')
     if 'GUNIFLASK_LOG_DIR' not in os.environ:
@@ -41,6 +46,22 @@ def set_environ():
 
 def _get_project_name():
     return os.environ.get('GUNIFLASK_PROJECT_NAME')
+
+
+def _infer_project_name(home_dir):
+    init_file = join(home_dir, '.guniflask-init.json')
+    if isfile(init_file):
+        try:
+            with open(init_file, 'r') as f:
+                data = json.load(f)
+            project_name = data.get('project_name')
+            if project_name and isdir(join(home_dir, project_name)):
+                return project_name
+        except Exception:
+            pass
+    for d in os.listdir(home_dir):
+        if not d.startswith('.') and isfile(join(home_dir, d, 'app.py')):
+            return d
 
 
 class InitDb(Command):
