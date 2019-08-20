c91d2c44d7d1d239b4496229c08b21fbfb5795db
jadbin <jadbin.com@hotmail.com>
2019-08-20 14:29:39 +0800

    only & only not None

--- guniflask/model.py
+++ guniflask/model.py
@@ -5,48 +5,63 @@ import datetime as dt
 import sqlalchemy
 
 
-def model_to_dict(model, ignore=None):
+def model_to_dict(model, ignore=None, only=None, only_not_none=False):
     col_attrs = sqlalchemy.inspect(model).mapper.column_attrs
     d = {}
     tz_info = dt.datetime.now(tz=dt.timezone.utc).astimezone().tzinfo
-    ignore_set = _get_ignore_set(ignore)
+    ignore_set = _get_field_set(ignore)
+    only_set = _get_field_set(only)
     for c in col_attrs:
         if c.key not in ignore_set:
+            if only and c.key not in only_set:
+                continue
             v = getattr(model, c.key)
             if isinstance(v, dt.datetime) and v.tzinfo is None:
                 v = v.replace(tzinfo=tz_info)
+            if only_not_none and v is None:
+                continue
             d[c.key] = v
     return d
 
 
-def dict_to_model(dict_obj, model_cls=None, ignore=None):
+def dict_to_model(dict_obj, model_cls=None, ignore=None, only=None, only_not_none=False):
     if model_cls is None:
         return dict(dict_obj)
     mapper = sqlalchemy.inspect(model_cls).mapper
     columns = mapper.columns
     col_attrs = mapper.column_attrs
-    ignore_set = _get_ignore_set(ignore)
+    ignore_set = _get_field_set(ignore)
+    only_set = _get_field_set(only)
     kwargs = {}
     for c in col_attrs:
         if c.key in dict_obj and c.key not in ignore_set:
+            if only and c.key not in only_set:
+                continue
             v = dict_obj[c.key]
             if isinstance(columns[c.key].type, sqlalchemy.DateTime):
                 v = string_to_datetime(v)
+            if only_not_none and v is None:
+                continue
             kwargs[c.key] = v
     model = model_cls(**kwargs)
     return model
 
 
-def update_model_by_dict(model, dict_obj, ignore=None):
+def update_model_by_dict(model, dict_obj, ignore=None, only=None, only_not_none=False):
     mapper = sqlalchemy.inspect(model).mapper
     columns = mapper.columns
     col_attrs = mapper.column_attrs
-    ignore_set = _get_ignore_set(ignore)
+    ignore_set = _get_field_set(ignore)
+    only_set = _get_field_set(only)
     for c in col_attrs:
         if c.key in dict_obj and c.key not in ignore_set:
+            if only and c.key not in only_set:
+                continue
             v = dict_obj[c.key]
             if isinstance(columns[c.key].type, sqlalchemy.DateTime):
                 v = string_to_datetime(v)
+            if only_not_none and v is None:
+                continue
             setattr(model, c.key, v)
 
 
@@ -59,10 +74,10 @@ def string_to_datetime(s):
     return s
 
 
-def _get_ignore_set(ignore):
-    if isinstance(ignore, str):
-        ignore = [i.strip() for i in ignore.split(',')]
-    return set(ignore if ignore else [])
+def _get_field_set(field):
+    if isinstance(field, str):
+        field = [i.strip() for i in field.split(',')]
+    return set(field or [])
 
 
 def wrap_model(model_cls):
