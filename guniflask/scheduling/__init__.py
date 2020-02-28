1c5c8d3df65d20b217afeeeb2470858a71adbc53
jadbin <jadbin.com@hotmail.com>
2020-02-28 12:23:12 +0800

    update background process

---
+++ guniflask/scheduling/__init__.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+
+from .background import *
+
+__all__ = background.__all__
---
+++ guniflask/utils/process.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+
+import os
+
+
+def daemonize():
+    if os.fork():
+        os._exit(0)
+    os.setsid()
+    if os.fork():
+        os._exit(0)
+    os.umask(0o22)
+    os.closerange(0, 3)
+    fd_null = os.open(os.devnull, os.O_RDWR)
+    if fd_null != 0:
+        os.dup2(fd_null, 0)
+    os.dup2(fd_null, 1)
+    os.dup2(fd_null, 2)
+
+
+def pid_exists(pid):
+    try:
+        os.kill(pid, 0)
+    except OSError:
+        return False
+    return True
