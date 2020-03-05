4e1c7a523cf280a8f72404d125f1f46b7ab9cec1
jadbin <jadbin.com@hotmail.com>
2020-03-06 07:12:03 +0800

    fix string utils

--- guniflask/utils/string.py
+++ guniflask/utils/string.py
@@ -4,23 +4,27 @@ import re
 
 _camelcase_invalid_chars = re.compile(r'[^a-zA-Z\d]')
 
-_name_invalid_chars = re.compile(r'[^a-zA-Z\d\-_]')
+_camelcase_split_chars = re.compile(r'(.)([A-Z][a-z]+)')
+_camelcase_split_chars2 = re.compile(r'([a-z0-9])([A-Z])')
 
 
 def string_camelcase(s):
-    return _camelcase_invalid_chars.sub('', s.title())
+    a = _camelcase_invalid_chars.split(s)
+    return ''.join([(i[0].upper() + i[1:]) for i in a if i])
 
 
 def string_lowercase_hyphen(s):
-    return _name_invalid_chars.sub('', s.strip().replace(' ', '-').lower()).replace('_', '-')
+    return string_lowercase_underscore(s).replace('_', '-')
 
 
 def string_lowercase_underscore(s):
-    return _name_invalid_chars.sub('', s.strip().replace(' ', '_').lower()).replace('-', '_')
+    s = string_camelcase(s)
+    s = _camelcase_split_chars.sub(r'\1_\2', s)
+    return _camelcase_split_chars2.sub(r'\1_\2', s).lower()
 
 
 def string_uppercase_underscore(s):
-    return _name_invalid_chars.sub('', s.strip().replace(' ', '_').upper()).replace('-', '_')
+    return string_lowercase_underscore(s).upper()
 
 
 def comma_delimited_list_to_set(s):
--- tests/test_string_utils.py
+++ tests/test_string_utils.py
@@ -4,16 +4,21 @@ from guniflask.utils.string import *
 
 
 def test_string_camelcase():
-    assert string_camelcase('ABc-dE FgH') == 'AbcDeFgh'
+    assert string_camelcase('get_http_response') == 'GetHttpResponse'
+    assert string_camelcase('meta-data') == 'MetaData'
+    assert string_camelcase('N2O') == 'N2O'
 
 
 def test_string_lowercase_hyphen():
-    assert string_lowercase_hyphen(' AB_cD-Ef gh ') == 'ab-cd-ef-gh'
+    assert string_lowercase_hyphen('getHTTPResponse') == 'get-http-response'
+    assert string_lowercase_hyphen('MetaData') == 'meta-data'
 
 
 def test_string_lowercase_underscore():
-    assert string_lowercase_underscore(' AB_cD-Ef gh ') == 'ab_cd_ef_gh'
+    assert string_lowercase_underscore('getHTTPResponse') == 'get_http_response'
+    assert string_lowercase_underscore('MetaData') == 'meta_data'
 
 
 def test_string_uppercase_underscore():
-    assert string_uppercase_underscore(' AB_cD-Ef gh ') == 'AB_CD_EF_GH'
+    assert string_uppercase_underscore('getHTTPResponse') == 'GET_HTTP_RESPONSE'
+    assert string_uppercase_underscore('MetaData') == 'META_DATA'
