84c29ac47e0f6fca369bb4ed6350992ed218a643
jadbin <jadbin.com@hotmail.com>
2020-03-06 16:29:46 +0800

    update example

--- guniflask/templates/project/{{project_name}}/examples/hello_world.py
+++ guniflask/templates/project/{{project_name}}/examples/hello_world.py
@@ -4,12 +4,14 @@ import json
 from os.path import join
 from functools import wraps
 
-from flask import request, abort, jsonify, render_template, current_app
+from flask import request, abort, jsonify, render_template
 
 from guniflask.config.template import _template_folder
 from guniflask.config import settings
 from guniflask.security import roles_required
+from guniflask.security import jwt_manager
 from guniflask.web import blueprint, get_route, route
+from guniflask.context import service
 
 static_folder = join(_template_folder, 'static')
 
@@ -24,15 +26,24 @@ def debug_only(func):
     return wrapper
 
 
-accounts = {
-    'root': {
-        'password': '123456',
-        'authorities': ['role_admin', 'role_user']
+@service
+class LoginService:
+    accounts = {
+        'root': {
+            'password': '123456',
+            'authorities': ['role_admin', 'role_user']
+        }
     }
-}
+
+    def login(self, username, password):
+        if username in self.accounts and password == self.accounts[username]['password']:
+            token = jwt_manager.create_access_token(self.accounts[username]['authorities'],
+                                                    username=username)
+            return {'access_token': token,
+                    'username': username}
 
 
-@blueprint(url_prefix='/hello-world',
+@blueprint('/hello-world',
            template_folder=join(_template_folder, 'hello_world'),
            static_folder=static_folder,
            static_url_path='/static')
@@ -41,8 +52,8 @@ class HelloWorld:
     An example
     """
 
-    def __init__(self):
-        self.jwt_manager = current_app.extensions.get('jwt_manager')
+    def __init__(self, login_service: LoginService):
+        self.login_service = login_service
 
     @get_route('/')
     def home_page(self):
@@ -62,11 +73,10 @@ class HelloWorld:
         if data is not None:
             username = data.get('username')
             password = data.get('password')
-            if username in accounts and password == accounts[username]['password']:
-                token = self.jwt_manager.create_access_token(accounts[username]['authorities'],
-                                                             username=username)
-                return jsonify({'access_token': token,
-                                'username': username})
+            result = self.login_service.login(username, password)
+            if result is None:
+                return abort(401)
+            return jsonify(result)
         return abort(403)
 
     @get_route('/settings')
@@ -83,6 +93,14 @@ class HelloWorld:
         """
         Get settings of app
         """
+
+        def is_jsonable(v):
+            try:
+                json.dumps(v)
+            except Exception:
+                return False
+            return True
+
         s = {}
         for k, v in settings.items():
             if is_jsonable(v):
@@ -90,11 +108,3 @@ class HelloWorld:
         app_settings = [{'key': i, 'value': j} for i, j in s.items()]
         app_settings.sort(key=lambda k: k['key'])
         return render_template('settings_table.html', app_settings=app_settings)
-
-
-def is_jsonable(v):
-    try:
-        json.dumps(v)
-    except Exception:
-        return False
-    return True
