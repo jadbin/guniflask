226383b5e2fde5ee9e5bfc3ebec12f6bc84de45e
jadbin <jadbin.com@hotmail.com>
2020-03-13 00:25:55 +0800

    global singleton on server

--- guniflask/web/__init__.py
+++ guniflask/web/__init__.py
@@ -3,9 +3,11 @@
 from .bind_annotation import *
 from .blueprint_post_processor import *
 from .context import *
+from .global_singleton_annotation import *
 from .scheduling_config import *
 
 __all__ = (bind_annotation.__all__ +
            blueprint_post_processor.__all__ +
            context.__all__ +
+           global_singleton_annotation.__all__ +
            scheduling_config.__all__)
---
+++ guniflask/web/global_singleton_annotation.py
@@ -0,0 +1,53 @@
+# coding=utf-8
+
+import os
+import tempfile
+from os.path import join, exists
+import fcntl
+
+from guniflask.annotation.core import AnnotationMetadata
+from guniflask.context.annotation import conditional
+from guniflask.context.condition import Condition, ConditionContext
+
+__all__ = ['global_singleton']
+
+
+class GlobalSingletonCondition(Condition):
+    global_singleton_conditions = {}
+
+    def matches(self, context: ConditionContext, metadata: AnnotationMetadata) -> bool:
+        singleton_id = self.generate_singleton_id(metadata)
+        if singleton_id in self.global_singleton_conditions:
+            return True
+
+        temp_dir = join(tempfile.gettempdir(), 'guniflask', 'global_singleton_lock')
+        if not exists(temp_dir):
+            os.makedirs(temp_dir, exist_ok=True)
+
+        fd = open(join(temp_dir, singleton_id), 'w')
+        try:
+            fcntl.flock(fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        except IOError:
+            return False
+        self.global_singleton_conditions[singleton_id] = fd
+        return True
+
+    def generate_singleton_id(self, metadata: AnnotationMetadata) -> str:
+        obj = metadata.source
+        if hasattr(obj, '__module__'):
+            module_name = obj.__module__
+        else:
+            module_name = None
+        if hasattr(obj, '__class__'):
+            class_name = obj.__class__.__name__
+        else:
+            class_name = None
+        if hasattr(obj, '__name__'):
+            obj_name = obj.__name__
+        else:
+            obj_name = None
+        return '{}.{}.{}'.format(module_name, class_name, obj_name)
+
+
+def global_singleton(func):
+    return conditional(GlobalSingletonCondition)(func)
