67c3188c4a4375f8bf9b158d1eb11bd03767b10d
jadbin <jadbin.com@hotmail.com>
2020-03-12 22:53:47 +0800

    update blueprint post processor

--- guniflask/web/blueprint_post_processor.py
+++ guniflask/web/blueprint_post_processor.py
@@ -10,8 +10,6 @@ __all__ = ['BlueprintPostProcessor']
 
 
 class BlueprintPostProcessor(BeanPostProcessor):
-    BLUEPRINT = '__blueprint'
-
     def __init__(self, app: Flask):
         self.app = app
 
@@ -19,15 +17,14 @@ class BlueprintPostProcessor(BeanPostProcessor):
         bean_type = bean.__class__
         annotation = AnnotationUtils.get_annotation(bean_type, Blueprint)
         if annotation is not None:
-            if not hasattr(bean, self.BLUEPRINT):
-                options = annotation['options'] or {}
-                b = FlaskBlueprint(bean_name, bean.__module__,
-                                   url_prefix=annotation['url_prefix'], **options)
-                for m in dir(bean):
-                    method = getattr(bean, m)
-                    a = AnnotationUtils.get_annotation(method, Route)
-                    if a is not None:
-                        rule_options = a['options'] or {}
-                        b.add_url_rule(a['rule'], endpoint=method.__name__, view_func=method, **rule_options)
-                self.app.register_blueprint(b)
+            options = annotation['options'] or {}
+            b = FlaskBlueprint(bean_name, bean.__module__,
+                               url_prefix=annotation['url_prefix'], **options)
+            for m in dir(bean):
+                method = getattr(bean, m)
+                a = AnnotationUtils.get_annotation(method, Route)
+                if a is not None:
+                    rule_options = a['options'] or {}
+                    b.add_url_rule(a['rule'], endpoint=method.__name__, view_func=method, **rule_options)
+            self.app.register_blueprint(b)
         return bean
