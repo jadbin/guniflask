c0b44030834886f2ce0cc8236d033c4c93e156a5
jadbin <jadbin.com@hotmail.com>
2020-03-13 23:43:22 +0800

    update example

--- guniflask/templates/project/{{project_name}}/examples/hello_world.py
+++ guniflask/templates/project/{{project_name}}/examples/hello_world.py
@@ -1,19 +1,16 @@
 # coding=utf-8
 
-from os.path import join
-
 from flask import render_template
 
 from guniflask.config.template import _template_folder
 from guniflask.web import blueprint, get_route
 
 
-@blueprint('/hello-world',
-           template_folder=join(_template_folder, 'hello_world'))
+@blueprint('/hello-world', template_folder=_template_folder)
 class HelloWorld:
     @get_route('/')
     def home_page(self):
         """
         Home page
         """
-        return render_template('index.html')
+        return render_template('hello_world/index.html')
