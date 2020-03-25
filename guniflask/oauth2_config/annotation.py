065a1ce588decc8b349ad2e61eb0a7aa395e8c24
jadbin <jadbin.com@hotmail.com>
2020-03-25 13:43:19 +0800

    add client details service configuration

--- guniflask/oauth2_config/annotation.py
+++ guniflask/oauth2_config/annotation.py
@@ -3,13 +3,15 @@
 from guniflask.annotation.annotation_utils import AnnotationUtils
 from guniflask.context.annotation import Include
 from guniflask.oauth2_config.authorization_server_endpoints import AuthorizationServerEndpointsConfiguration
+from guniflask.oauth2_config.client_details_service import ClientDetailsServiceConfiguration
 
 __all__ = ['enable_authorization_server']
 
 
 def enable_authorization_server():
     def wrap_func(func):
-        values = [AuthorizationServerEndpointsConfiguration]
+        values = [AuthorizationServerEndpointsConfiguration,
+                  ClientDetailsServiceConfiguration]
         AnnotationUtils.add_annotation(func, Include(values))
         return func
 
--- guniflask/oauth2_config/client_details_service.py
+++ guniflask/oauth2_config/client_details_service.py
@@ -1,14 +1,26 @@
 # coding=utf-8
 
-from guniflask.context.annotation import configuration
+from guniflask.context.annotation import configuration, bean
+from guniflask.oauth2.client_details_service import ClientDetailsService
+from guniflask.oauth2_config.authorization_server import AuthorizationServerConfigurer
 
 __all__ = ['ClientDetailsServiceConfigurer', 'ClientDetailsServiceConfiguration']
 
 
 class ClientDetailsServiceConfigurer:
-    pass
+    def __init__(self):
+        self.client_details_service: ClientDetailsService = None
+
+    def get_client_details_service(self):
+        return self.get_client_details_service()
 
 
 @configuration
 class ClientDetailsServiceConfiguration:
-    pass
+    def __init__(self, authorization_server_configurer: AuthorizationServerConfigurer):
+        self.service = ClientDetailsServiceConfigurer()
+        authorization_server_configurer.configure_client_details_service(self.service)
+
+    @bean
+    def client_details_service(self):
+        return self.service.get_client_details_service()
