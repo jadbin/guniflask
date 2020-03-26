56dd51683cde70fc17a5119452cc4a229aa66d3d
jadbin <jadbin.com@hotmail.com>
2020-03-26 09:54:03 +0800

    enable resource server

--- guniflask/oauth2_config/annotation.py
+++ guniflask/oauth2_config/annotation.py
@@ -4,14 +4,25 @@ from guniflask.annotation.annotation_utils import AnnotationUtils
 from guniflask.context.annotation import Include
 from guniflask.oauth2_config.authorization_server_endpoints import AuthorizationServerEndpointsConfiguration
 from guniflask.oauth2_config.client_details_service import ClientDetailsServiceConfiguration
+from guniflask.oauth2_config.resource_server import ResourceServerConfiguration
 
-__all__ = ['enable_authorization_server']
+__all__ = ['enable_authorization_server',
+           'enable_resource_server']
 
 
 def enable_authorization_server():
     def wrap_func(func):
         values = [AuthorizationServerEndpointsConfiguration,
                   ClientDetailsServiceConfiguration]
+        AnnotationUtils.add_annotation(func, Include(values))
+        return func
+
+    return wrap_func
+
+
+def enable_resource_server():
+    def wrap_func(func):
+        values = [ResourceServerConfiguration]
         AnnotationUtils.add_annotation(func, Include(values))
         return func
 
