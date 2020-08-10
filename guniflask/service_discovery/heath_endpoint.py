e04e2e382a07dce1b24ac3a5440abb54bec10cc9
jadbin <jadbin.com@hotmail.com>
2020-08-10 11:45:47 +0800

    fix checking active profiles

--- guniflask/service_discovery/heath_endpoint.py
+++ guniflask/service_discovery/heath_endpoint.py
@@ -13,14 +13,13 @@ class HealthEndpoint:
 
     @get_route('/health')
     def health_check(self, active_profiles: str = None):
-        if active_profiles and not self._check_active_profiles(active_profiles):
+        if not self._check_active_profiles(active_profiles):
             abort(400, 'Active profiles check failed')
         return {'status': 'UP'}
 
     def _check_active_profiles(self, active_profile: str):
-        if active_profile is None:
-            if not settings['active_profiles']:
-                return True
+        if not active_profile:
+            return not settings['active_profiles']
 
         profiles = set(active_profile.split(','))
         app_profiles = set(settings['active_profiles'] or [])
