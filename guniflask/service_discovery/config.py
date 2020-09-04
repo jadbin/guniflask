f4bf873018b6bfc82882f12593465cb688806816
jadbin <jadbin.com@hotmail.com>
2020-09-04 15:29:43 +0800

    update service discovery

--- guniflask/service_discovery/config.py
+++ guniflask/service_discovery/config.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 
 import logging
+from abc import ABCMeta, abstractmethod
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
@@ -9,7 +10,7 @@ from guniflask.config.app_config import settings, Settings
 from guniflask.utils.network import get_local_ip_address
 from guniflask.service_discovery.consul import ConsulClient, ConsulClientError
 from guniflask.distributed.local_lock import ServiceLock
-from guniflask.context.annotation import configuration, bean
+from guniflask.context.annotation import configuration, bean, condition_on_setting
 from guniflask.service_discovery.heath_endpoint import HealthEndpoint
 from guniflask.service_discovery.discovery_client import DiscoveryClient
 from guniflask.service_discovery.load_balancer_client import LoadBalancerClient, LoadBalancedRequest
@@ -17,47 +18,46 @@ from guniflask.service_discovery.load_balancer_client import LoadBalancerClient,
 log = logging.getLogger(__name__)
 
 
+class ServiceDiscoveryConfigurer(metaclass=ABCMeta):
+    @abstractmethod
+    def configure(self, service_name: str, app_settings: Settings) -> bool:
+        pass
+
+    @property
+    @abstractmethod
+    def discovery_client(self) -> DiscoveryClient:
+        pass
+
+    @property
+    @abstractmethod
+    def load_balancer_client(self) -> LoadBalancerClient:
+        pass
+
+
+@condition_on_setting('guniflask.consul')
 @configuration
-class ServiceDiscoveryConfiguration:
-    SERVICE_REGISTRY_NAMES = ['consul']
+class ConsulConfigurer(ServiceDiscoveryConfigurer):
 
     def __init__(self):
-        self.service_name = settings['project_name']
         self._discovery_client: DiscoveryClient = None
         self._load_balancer_client: LoadBalancerClient = None
+        self._register_scheduler = None
+        self._service_name: str = None
 
-        self._register_scheduler = BackgroundScheduler()
-        self._register_scheduler.start(paused=False)
-        self._register_lock = ServiceLock('service_registration_lock')
-        self._auto_register()
-
-    @bean
-    def heath_endpoint(self) -> HealthEndpoint:
-        endpoint = HealthEndpoint()
-        return endpoint
-
-    @bean
+    @property
     def discovery_client(self) -> DiscoveryClient:
         return self._discovery_client
 
-    @bean
+    @property
     def load_balancer_client(self) -> LoadBalancerClient:
         return self._load_balancer_client
 
-    @bean
-    def load_balanced_request(self) -> LoadBalancedRequest:
-        if self._load_balancer_client is not None:
-            return LoadBalancedRequest(self._load_balancer_client)
-
-    def _auto_register(self):
-        if not self._register_lock.acquire():
-            return
-
-        app_settings = settings._get_current_object()
-        for name in self.SERVICE_REGISTRY_NAMES:
-            config = app_settings.get_by_prefix('guniflask.{}'.format(name))
-            if config is not None:
-                getattr(self, '_{}_register'.format(name))(config, app_settings)
+    def configure(self, service_name: str, app_settings: Settings):
+        config = app_settings.get_by_prefix('guniflask.consul')
+        self._service_name = service_name
+        self._register_scheduler = BackgroundScheduler()
+        self._register_scheduler.start(paused=False)
+        self._consul_register(config, app_settings)
 
     def _consul_register(self, config: dict, app_settings: Settings):
         consul = self._consul_client(config)
@@ -75,7 +75,7 @@ class ServiceDiscoveryConfiguration:
 
     def _consul_client(self, config: dict) -> ConsulClient:
         client_config = {}
-        for i in ['host', 'port', 'dns_port']:
+        for i in ['host', 'port', 'dns_port', 'scheme']:
             if i in config:
                 client_config[i] = config[i]
         return ConsulClient(**client_config)
@@ -83,13 +83,13 @@ class ServiceDiscoveryConfiguration:
     def _do_consul_register(self, consul: ConsulClient, app_settings: Settings):
         local_ip = get_local_ip_address()
         port = app_settings['port']
-        service_id = '{}-{}-{}'.format(self.service_name, local_ip, port)
-        heath_url = 'http://{}:{}/health?' \
+        service_id = '{}-{}-{}'.format(app_settings['project_name'], local_ip, port)
+        heath_url = 'http://{}:{}/_health?' \
                     'name={}&active_profiles={}'.format(local_ip, port,
                                                         app_settings['project_name'],
                                                         ','.join(app_settings['active_profiles']))
         try:
-            consul.register_service(self.service_name,
+            consul.register_service(app_settings['project_name'],
                                     service_id=service_id,
                                     address=local_ip,
                                     port=port,
@@ -100,3 +100,45 @@ class ServiceDiscoveryConfiguration:
                                                             deregister_after='10m'))
         except ConsulClientError as e:
             log.error('Failed to register service to Consul at %s:%s: %s', consul.host, consul.port, e)
+
+
+@configuration
+class ServiceDiscoveryConfiguration:
+    SERVICE_DISCOVERY = {
+        'consul': ConsulConfigurer,
+    }
+
+    def __init__(self, service_discovery_configurer: ServiceDiscoveryConfigurer = None):
+        self._service_discovery_configurer = service_discovery_configurer
+
+        self._register_lock = ServiceLock('service_registration_lock')
+        self._auto_register()
+
+    @bean
+    def heath_endpoint(self) -> HealthEndpoint:
+        endpoint = HealthEndpoint()
+        return endpoint
+
+    @bean
+    def discovery_client(self) -> DiscoveryClient:
+        if self._service_discovery_configurer:
+            return self._service_discovery_configurer.discovery_client
+
+    @bean
+    def load_balancer_client(self) -> LoadBalancerClient:
+        if self._service_discovery_configurer:
+            return self._service_discovery_configurer.load_balancer_client
+
+    @bean
+    def load_balanced_request(self) -> LoadBalancedRequest:
+        if self._load_balancer_client is not None:
+            return LoadBalancedRequest(self._load_balancer_client)
+
+    def _auto_register(self):
+        if not self._register_lock.acquire():
+            return
+
+        app_settings = settings._get_current_object()
+        service_name = app_settings['project_name']
+        if self._service_discovery_configurer is not None:
+            self._service_discovery_configurer.configure(service_name, app_settings)
--- guniflask/service_discovery/heath_endpoint.py
+++ guniflask/service_discovery/heath_endpoint.py
@@ -10,10 +10,14 @@ from guniflask.config.app_config import settings
 class HealthEndpoint:
 
     @get_route('/health')
-    def health_check(self, name: str = None, active_profiles: str = None):
-        if not self._check_info(name, active_profiles):
-            abort(400, 'Active profiles check failed')
+    def health_check(self):
         return {'status': 'UP'}
+
+    @get_route('/_health')
+    def internal_health_check(self, name: str = None, active_profiles: str = None):
+        if not self._check_info(name, active_profiles):
+            abort(400, 'Signature check failed')
+        return self.health_check()
 
     def _check_info(self, name: str, active_profile: str):
         if name != settings['project_name']:
@@ -22,8 +26,8 @@ class HealthEndpoint:
         if not active_profile:
             return not settings['active_profiles']
 
-        profiles = set(active_profile.split(','))
-        app_profiles = set(settings['active_profiles'] or [])
+        profiles = active_profile.split(',')
+        app_profiles = settings['active_profiles'] or []
         if profiles == app_profiles:
             return True
 
