eb906e5726e56ac777697d9da879cea619cef61d
jadbin <jadbin.com@hotmail.com>
2020-11-05 01:49:32 +0800

    default home

--- guniflask/test/env.py
+++ guniflask/test/env.py
@@ -1,10 +1,19 @@
 # coding=utf-8
 
 import os
+from os.path import dirname, curdir, join, exists, abspath
 
 from guniflask.config.env import load_app_env
 
 
 def set_test_env():
+    home = os.environ.get('GUNIFLASK_HOME')
+    if home is None:
+        home = abspath(curdir)
+        while True:
+            if not exists(join(home, '__init__.py')):
+                break
+            home = dirname(home)
+        os.environ['GUNIFLASK_HOME'] = home
     os.environ.setdefault('GUNIFLASK_ACTIVE_PROFILES', 'dev')
     load_app_env()
