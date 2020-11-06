036bd71c9c621115fd8d3ac9eb045cf9fce99300
jadbin <jadbin.com@hotmail.com>
2020-11-06 14:00:08 +0800

    data model

---
+++ guniflask/data_model/base.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+
+from typing import get_type_hints, Any
+
+from guniflask.orm.base_model import BaseModelMixin
+
+
+class DataModel:
+    def __init__(self, **data: Any):
+        cls = self.__class__
+        for k in get_type_hints(cls):
+            if k in data:
+                setattr(self, k, data[k])
+            elif not hasattr(self, k):
+                setattr(self, k, None)
+
+    def to_dict(self):
+        cls = self.__class__
+        d = {}
+        for k in get_type_hints(cls):
+            d[k] = getattr(self, k, None)
+        return d
+
+    @classmethod
+    def from_dict(cls, obj: dict):
+        d = cls()
+        for k in get_type_hints(cls):
+            if k in obj:
+                setattr(d, k, obj[k])
+        return d
+
+    @classmethod
+    def from_orm(cls, obj: BaseModelMixin):
+        return cls.from_dict(obj.to_dict())
---
+++ tests/test_data_model/test_base.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+
+from guniflask.data_model.base import DataModel
+
+
+class Person(DataModel):
+    name: str
+    age: int
+
+
+class Student(Person):
+    graduate: bool
+
+
+def test_create_data_model():
+    student = Student(name='Bob', graduate=True)
+    assert student.name == 'Bob'
+    assert student.age is None
+    assert student.graduate is True
+
+
+def test_data_model_from_dict():
+    student = Student.from_dict(dict(name='Bob', graduate=True))
+    assert student.name == 'Bob'
+    assert student.age is None
+    assert student.graduate is True
+
+
+def test_data_model_to_dict():
+    d = Student.from_dict(dict(name='Bob', graduate=True)).to_dict()
+    assert d['name'] == 'Bob'
+    assert d['age'] is None
+    assert d['graduate'] is True
