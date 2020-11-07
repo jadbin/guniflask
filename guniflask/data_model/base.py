d0c98bcd92d2219d417dbf35e30b71043a72afca
jadbin <jadbin.com@hotmail.com>
2020-11-08 01:23:15 +0800

    fix data model to dict

--- guniflask/data_model/base.py
+++ guniflask/data_model/base.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 
+from enum import Enum
 from typing import get_type_hints, Any
 
 from guniflask.data_model.mapping import map_json
@@ -15,17 +16,28 @@ class DataModel:
             elif not hasattr(self, k):
                 setattr(self, k, None)
 
-    def to_dict(self):
+    def to_dict(self) -> dict:
         cls = self.__class__
         d = {}
         for k in get_type_hints(cls):
-            d[k] = getattr(self, k, None)
+            d[k] = self._get_value(getattr(self, k, None))
         return d
 
+    def _get_value(self, v: Any) -> Any:
+        if isinstance(v, DataModel):
+            return v.to_dict()
+        if isinstance(v, dict):
+            return {_k: self._get_value(_v) for _k, _v in v.items()}
+        if isinstance(v, (list, set, tuple)):
+            return [self._get_value(_v) for _v in v]
+        if isinstance(v, Enum):
+            return v.value
+        return v
+
     @classmethod
-    def from_dict(cls, obj: dict):
+    def from_dict(cls, obj: dict) -> 'DataModel':
         return map_json(obj, cls)
 
     @classmethod
-    def from_orm(cls, obj: BaseModelMixin):
+    def from_orm(cls, obj: BaseModelMixin) -> 'DataModel':
         return cls.from_dict(obj.to_dict())
--- tests/test_data_model/test_base.py
+++ tests/test_data_model/test_base.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+from typing import List
+
 from guniflask.data_model import DataModel
 
 
@@ -31,3 +33,53 @@ def test_data_model_to_dict():
     assert d['name'] == 'Bob'
     assert d['age'] is None
     assert d['graduate'] is True
+
+
+class User(DataModel):
+    name: str
+
+
+class Article(DataModel):
+    title: str
+    author: User
+
+
+def test_data_model_from_dict_with_many_to_one():
+    d = Article.from_dict(dict(title='Title', author=dict(name='Bob')))
+    assert d.title == 'Title'
+    assert d.author.name == 'Bob'
+
+
+def test_data_model_to_dict_with_many_to_one():
+    d = Article.from_dict(dict(title='Title', author=dict(name='Bob'))).to_dict()
+    assert d == {
+        'title': 'Title',
+        'author': {
+            'name': 'Bob',
+        },
+    }
+
+
+class ArticleSet(DataModel):
+    name: str
+    articles: List[Article]
+
+
+def test_data_model_from_dict_with_one_to_many():
+    d = ArticleSet.from_dict(dict(name='Name', articles=[dict(title='Title')]))
+    assert d.name == 'Name'
+    assert len(d.articles) == 1
+    assert d.articles[0].title == 'Title'
+
+
+def test_data_model_to_dict_with_one_to_many():
+    d = ArticleSet.from_dict(dict(name='Name', articles=[dict(title='Title')])).to_dict()
+    assert d == {
+        'name': 'Name',
+        'articles': [
+            {
+                'title': 'Title',
+                'author': None,
+            }
+        ],
+    }
