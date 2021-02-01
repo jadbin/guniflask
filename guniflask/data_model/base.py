fbca5eda345d7cb72824272a64f90d2b112f2930
jadbin <jadbin.com@hotmail.com>
2021-02-01 10:49:39 +0800

    fix to_dict

--- guniflask/data_model/base.py
+++ guniflask/data_model/base.py
@@ -1,4 +1,5 @@
-from typing import Union, List, Set
+from enum import Enum
+from typing import Union, List, Set, Any
 
 from pydantic import BaseModel
 
@@ -9,15 +10,68 @@ from guniflask.utils.rule import make_ignore_rule_for_field, make_only_rule_for_
 class DataModel(BaseModel):
     def to_dict(
             self,
-            ignore: Union[str, List[str]] = None,
-            only: Union[str, List[str]] = None,
+            ignore: Union[str, List[str], Set[str]] = None,
+            only: Union[str, List[str], Set[str]] = None,
     ) -> dict:
-        kwargs = {}
-        if ignore:
-            kwargs['exclude'] = _make_ignore_rule(ignore)
-        if only:
-            kwargs['include'] = _make_only_rule(only)
-        return self.dict()
+        ignore = make_ignore_rule_for_field(ignore)
+        only = make_only_rule_for_field(only)
+        return self.__to_dict(ignore, only)
+
+    def __to_dict(
+            self,
+            ignore: Set[str],
+            only: Set[str],
+            _prefix: str = '',
+    ) -> dict:
+        data = {}
+        for k, v in self.__dict__.items():
+            if _in_set(_prefix, k, ignore):
+                continue
+            if only and not _in_set(_prefix, k, only):
+                continue
+            data[k] = self.__get_value_from(
+                v,
+                ignore=ignore,
+                only=only,
+                _prefix=_new_prefix(_prefix, k),
+            )
+        return data
+
+    def __get_value_from(
+            self,
+            v: Any,
+            ignore: Set[str],
+            only: Set[str],
+            _prefix: str = '',
+    ) -> Any:
+        if isinstance(v, DataModel):
+            return v.__to_dict(ignore=ignore, only=only, _prefix=_prefix)
+        if isinstance(v, dict):
+            data = {}
+            for _k, _v in v.items():
+                if _in_set(_prefix, _k, ignore):
+                    continue
+                if only and not _in_set(_prefix, _k, only):
+                    continue
+                data[_k] = self.__get_value_from(
+                    _v,
+                    ignore=ignore,
+                    only=only,
+                    _prefix=_new_prefix(_prefix, _k),
+                )
+            return data
+        if isinstance(v, (list, set, tuple)):
+            return [
+                self.__get_value_from(
+                    _v,
+                    ignore=ignore,
+                    only=only,
+                    _prefix=_prefix,
+                ) for _v in v
+            ]
+        if isinstance(v, Enum):
+            return v.value
+        return v
 
     @classmethod
     def from_dict(cls, obj: dict):
@@ -27,9 +81,9 @@ class DataModel(BaseModel):
     def from_orm(
             cls,
             obj: BaseModelMixin,
-            ignore: Union[str, List[str]] = None,
-            only: Union[str, List[str]] = None,
-            include: Union[str, List[str]] = None,
+            ignore: Union[str, List[str], Set[str]] = None,
+            only: Union[str, List[str], Set[str]] = None,
+            include: Union[str, List[str], Set[str]] = None,
     ):
         return cls.from_dict(
             obj.to_dict(
@@ -54,9 +108,19 @@ def _expand_rule_set(rule: Set[str]) -> dict:
     return result
 
 
-def _make_ignore_rule(ignore: Union[str, List[str]]) -> dict:
+def _make_ignore_rule(ignore: Union[str, List[str], Set[str]]) -> dict:
     return _expand_rule_set(make_ignore_rule_for_field(ignore))
 
 
-def _make_only_rule(only: Union[str, List[str]]) -> dict:
+def _make_only_rule(only: Union[str, List[str], Set[str]]) -> dict:
     return _expand_rule_set(make_only_rule_for_field(only))
+
+
+def _in_set(prefix: str, key: str, s: set):
+    return _new_prefix(prefix, key) in s
+
+
+def _new_prefix(prefix: str, key: str):
+    if not prefix:
+        return key
+    return prefix + '.' + key
--- tests/test_data_model/test_base.py
+++ tests/test_data_model/test_base.py
@@ -71,7 +71,9 @@ def test_data_model_from_dict_with_one_to_many():
 
 
 def test_data_model_to_dict_with_one_to_many():
-    d = ArticleSet.from_dict(dict(name='Name', articles=[dict(title='Title')])).to_dict()
+    article_set = ArticleSet.from_dict(dict(name='Name', articles=[dict(title='Title')]))
+
+    d = article_set.to_dict()
     assert d == {
         'name': 'Name',
         'articles': [
@@ -80,4 +82,28 @@ def test_data_model_to_dict_with_one_to_many():
                 'author': None,
             }
         ],
+    }
+
+    d = article_set.to_dict(ignore='articles.author')
+    assert d == {
+        'name': 'Name',
+        'articles': [
+            {
+                'title': 'Title',
+            }
+        ],
+    }
+
+    d = article_set.to_dict(ignore='articles')
+    assert d == {
+        'name': 'Name',
+    }
+
+    d = article_set.to_dict(only='articles.title')
+    assert d == {
+        'articles': [
+            {
+                'title': 'Title',
+            }
+        ]
     }
