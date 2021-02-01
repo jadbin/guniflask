d648549ceee01208ea45e7379c90eaba08c27ffa
jadbin <jadbin.com@hotmail.com>
2021-02-01 10:49:19 +0800

    update api

--- guniflask/orm/base_model.py
+++ guniflask/orm/base_model.py
@@ -1,4 +1,4 @@
-from typing import Union, List
+from typing import Union, List, Set
 
 from sqlalchemy.orm import Query
 
@@ -12,23 +12,23 @@ class BaseModelMixin:
     def from_dict(
             cls,
             dict_obj: dict,
-            ignore: Union[str, List[str]] = None,
-            only: Union[str, List[str]] = None,
+            ignore: Union[str, List[str], Set[str]] = None,
+            only: Union[str, List[str], Set[str]] = None,
     ):
         return dict_to_model(dict_obj, cls, ignore=ignore, only=only)
 
     def to_dict(
             self,
-            ignore: Union[str, List[str]] = None,
-            only: Union[str, List[str]] = None,
-            include: Union[str, List[str]] = None,
+            ignore: Union[str, List[str], Set[str]] = None,
+            only: Union[str, List[str], Set[str]] = None,
+            include: Union[str, List[str], Set[str]] = None,
     ):
         return model_to_dict(self, ignore=ignore, only=only, include=include)
 
     def update_by_dict(
             self,
             dict_obj: dict,
-            ignore: Union[str, List[str]] = None,
-            only: Union[str, List[str]] = None,
+            ignore: Union[str, List[str], Set[str]] = None,
+            only: Union[str, List[str], Set[str]] = None,
     ):
         return update_model_by_dict(self, dict_obj, ignore=ignore, only=only)
